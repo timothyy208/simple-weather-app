848e63468337a09f0fb31945d5efe402eb9e5f3f
Timothy Yang <timothyy208@gmail.com>
2019-03-11 04:10:27 -0400

    7.4

--- WeatherGift/PageVC.swift
+++ WeatherGift/PageVC.swift
@@ -12,6 +12,10 @@ class PageVC: UIPageViewController {
 
     var currentPage = 0
     var locationsArray = ["Local City", "Sydney, Australia", "Accra, Ghana", "Uglich, Russia"]
+    var pageControl: UIPageControl!
+    var barButtonWidth: CGFloat = 44
+    var barButtonHeight: CGFloat = 44
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +23,25 @@ class PageVC: UIPageViewController {
         dataSource = self
 
         setViewControllers([createDetailVC(forPage: 0)], direction: .forward, animated: false, completion: nil)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        configurePageControl()
+    }
+    
+    func configurePageControl() {
+        let pageControlHeight: CGFloat = barButtonHeight
+        let pageControlWidth: CGFloat = view.frame.width - (barButtonWidth * 2)
+        
+        let safeHeight = view.frame.height - view.safeAreaInsets.bottom
+        
+        pageControl = UIPageControl(frame: CGRect(x: (view.frame.width - pageControlWidth)/2, y: safeHeight - pageControlHeight, width: pageControlWidth, height: pageControlHeight))
+        pageControl.pageIndicatorTintColor = UIColor.lightGray
+        pageControl.currentPageIndicatorTintColor =  UIColor.black
+        pageControl.numberOfPages = locationsArray.count
+        pageControl.currentPage = currentPage
+        view.addSubview(pageControl)
     }
     
     func createDetailVC(forPage page: Int) -> DetailVC {
@@ -55,5 +78,9 @@ extension PageVC: UIPageViewControllerDataSource, UIPageViewControllerDelegate{
         return nil
     }
     
-    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if let currentViewController = pageViewController.viewControllers?[0] as? DetailVC {
+            pageControl.currentPage = currentViewController.currentPage
+        }
+    }
 }
