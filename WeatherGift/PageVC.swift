e7e24faa937276ab47c2a8aafd7fc30435de805e
Timothy Yang <timothyy208@gmail.com>
2019-03-11 03:53:45 -0400

    7.3

--- WeatherGift/PageVC.swift
+++ WeatherGift/PageVC.swift
@@ -10,21 +10,50 @@ import UIKit
 
 class PageVC: UIPageViewController {
 
+    var currentPage = 0
+    var locationsArray = ["Local City", "Sydney, Australia", "Accra, Ghana", "Uglich, Russia"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        delegate = self
+        dataSource = self
 
-        // Do any additional setup after loading the view.
+        setViewControllers([createDetailVC(forPage: 0)], direction: .forward, animated: false, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func createDetailVC(forPage page: Int) -> DetailVC {
+        
+        currentPage = min(max(0, page), locationsArray.count - 1)
+        
+        let detailVC = storyboard!.instantiateViewController(withIdentifier: "DetailVC") as! DetailVC
+        
+        detailVC.locationsArray = locationsArray
+        detailVC.currentPage = currentPage
+        
+        return detailVC
+        
     }
-    */
 
+  
+}
+
+extension PageVC: UIPageViewControllerDataSource, UIPageViewControllerDelegate{
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        if let currentViewController = viewController as? DetailVC {
+            if currentViewController.currentPage < locationsArray.count-1 {
+                return createDetailVC(forPage: currentViewController.currentPage+1)
+            }
+        }
+        return nil
+    }
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        if let currentViewController = viewController as? DetailVC {
+            if currentViewController.currentPage > 0 {
+                return createDetailVC(forPage: currentViewController.currentPage-1)
+            }
+        }
+        return nil
+    }
+    
+    
 }
